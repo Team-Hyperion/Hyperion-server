c13ec1789a70e464a45acbb8f8b0dce87ad860e7
Johnny Cai <jaihysc@gmail.com>
2020-10-14 21:05:30 -0400

    Fixed test failure Connection.OpenOutFileNoDirectory

--- src/media/save.cpp
+++ src/media/save.cpp
@@ -9,7 +9,10 @@ using namespace hyperion;
 std::ofstream media::MakeSaveFile(const std::string& save_path) {
     const auto path = std::filesystem::path(save_path);
 
-    create_directories(path.parent_path());
+    const auto parent_path = path.parent_path();
+    if (!parent_path.empty()) {
+        create_directories(parent_path);
+    }
 
     auto of = std::ofstream(save_path, std::ios::binary);
     if (!of.is_open()) {
